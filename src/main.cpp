3d243a0ee414ae251e79ff962bbc792d401861a3
Siddhant Mishra <priyankmishra184@gmail.com>
2025-12-16 19:09:42 +0530

    create basic logger class using oop fundamental of(class and object)

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "Logger.h"
+
+int main(){
+    Logger logger;
+    logger.log("system is working good");
+    logger.log("user logged in successfully");
+    return 0;
+}
